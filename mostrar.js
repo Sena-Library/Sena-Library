124d731872685394e47317c03532c9a556a4e59e
Michael_Conde <michaelconde387@gmail.com>
2024-06-18 14:50:16 -0500

    Prototype

---
+++ mostrar.js
@@ -0,0 +1,11 @@
+var nombre = sessionStorage.getItem('nombre');
+var telefono = sessionStorage.getItem('telefono');
+var direccion = sessionStorage.getItem('direccion');
+var direccion = sessionStorage.getItem('direccion');
+
+
+// Mostrar los datos en los elementos HTML correspondientes
+document.getElementById('span').innerText = nombre;
+document.getElementById('span1').innerText = telefono;
+document.getElementById('span2').innerText = direccion;
+document.getElementById('span3').innerText = direccion;
---
+++ register.js
@@ -0,0 +1,20 @@
+function register() {
+    let name = document.getElementById("nombre").value;
+    let tel = document.getElementById("telefono").value;
+    let direcc = document.getElementById("direccion").value;
+    let password = document.getElementById("contraseña").value;
+   
+    if (name === "" || tel === "" || direcc === "" || password === "") {
+        alert("Por favor, rellene todos los campos");
+    } else {
+        // Guardar los datos en sessionStorage
+        sessionStorage.setItem('nombre', name);
+        sessionStorage.setItem('telefono', tel);
+        sessionStorage.setItem('direccion', direcc);
+        sessionStorage.setItem('contraseña', password);
+
+
+        // Redireccionar a la página "person.html"
+        window.location.href = "../index.html";
+    }
+}
